1b06f5c127b568844f9993abae7fe7ba7adf8882
pren1 <33309499+pren1@users.noreply.github.com>
2020-02-29 13:41:50 -0800

    initial comments

---
+++ index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+const io = require('socket.io-client')
+const socket = io('https://api.vtbs.moe')
+
+const IO_Server = require('socket.io')
+// const dispatch = new Server(9003, { serveClient: false })
+
+var app = express()
+var server = app.listen(9003, function(){
+  console.log("Node.js server created");
+})
+app.use(express.static('front-end'))
+var io_= IO_Server(server, {pingTimeout: 60000});
+
+io_.on("connection", function(socket) {
+  console.log("socket.io connected " + socket.id)
+  io_.send("Hello from node.js")
+})
+
+const { LiveWS } = require('bilibili-live-ws')
+const no = require('./env')
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const rooms = new Set()
+
+const openRoom = ({ roomid, mid }) => new Promise(resolve => {
+  console.log(`OPEN: ${roomid}`)
+  const live = new LiveWS(roomid)
+  const autorestart = setTimeout(() => {
+    console.log(`AUTORESTART: ${roomid}`)
+    live.close()
+  }, 1000 * 60 * 60 * 18)
+  let timeout = setTimeout(() => {
+    console.log(`TIMEOUT: ${roomid}`)
+    live.close()
+  }, 1000 * 45)
+  live.once('live', () => console.log(`LIVE: ${roomid}`))
+  live.on('DANMU_MSG', async ({ info }) => {
+    if (!info[0][9]) {
+      const message = info[1]
+      const mid = info[2][0]
+      const uname = info[2][1]
+      const timestamp = info[0][4]
+      io_.send({ message, roomid, mid, uname, timestamp})
+      // dispatch.emit('danmaku', { message, roomid, mid, uname, timestamp })
+      // const socket_sender = io("http://localhost:8080");
+      // socket_sender.emit('danmaku', { message, roomid, mid, uname, timestamp });
+      console.log({ message, roomid, mid, uname, timestamp })
+    }
+  })
+
+  live.on('heartbeat', () => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      console.log(`TIMEOUT: ${roomid}`)
+      live.close()
+    }, 1000 * 45)
+  })
+  live.on('close', () => {
+    clearTimeout(autorestart)
+    clearTimeout(timeout)
+    resolve({ roomid })
+  })
+  live.on('error', () => {
+    console.log(`ERROR: ${roomid}`)
+  })
+})
+
+const watch = async ({ roomid, mid }) => {
+  if (!rooms.has(roomid)) {
+    rooms.add(roomid)
+    console.log(`WATCH: ${roomid}`)
+    while (true) {
+      await openRoom({ roomid, mid })
+      console.log(`CLOSE: ${roomid}`)
+      await wait(50)
+      console.log(`REOPEN: ${roomid}`)
+    }
+  }
+}
+
+socket.on('info', async info => {
+  info
+    .filter(({ roomid }) => roomid)
+    .filter(({ roomid }) => !no.includes(roomid))
+    .forEach(({ roomid, mid }) => watch({ roomid, mid }))
+  console.log('REFRESH')
+})
