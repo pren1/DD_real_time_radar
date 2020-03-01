0955be56828b6d89285ec1388cef80928d93972b
ScilientHill <pren1@ucsc.edu>
2020-02-29 16:10:04 -0800

    sio in class

--- MongoDB.py
+++ MongoDB.py
@@ -3,18 +3,21 @@ import pymongo
 
 class MongoDB(object):
 	def __init__(self):
+		'To use this service, you need to install MongoDB first'
 		self.myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-		dblist = self.myclient.list_database_names()
-		if "danmaku_db" in dblist:
-			print("dataset has existed！")
-
 		self.mydb = self.myclient["danmaku_db"]
 		self.mycol = self.mydb["naive"]
 
 	def insert_one(self, my_dict):
+		'add one message into the database'
 		self.mycol.insert_one(my_dict)
 
+	def find_one(self):
+		for x in self.mycol.find({'uname': '栀夏暮年'}, {"_id": 0}):
+			print(x)
+
 if __name__ == '__main__':
-	mydict = {'message': '【诶…装备全让我卸下来了 没问题吗】', 'roomid': 21449083, 'mid': 393489, 'uname': '猫にこ', 'timestamp': 1583017729907}
+	# mydict = {'message': '【诶…装备全让我卸下来了 没问题吗】', 'roomid': 21449083, 'mid': 393489, 'uname': '猫にこ', 'timestamp': 1583017729907}
 	db = MongoDB()
-	db.insert_one(mydict)
+	# db.insert_one(mydict)
+	db.find_one()
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Detect the danmaku data in realtime, then track the target Simultaneous interpre
 
 本项目继承自：[bilibili-vtuber-live-danmaku-relay](https://github.com/dd-center/bilibili-vtuber-live-danmaku-relay)
 
-1. ✅ 监听所有vtuber直播间弹幕 已完成，by Kinori
-2. Python端构建数据库，并将弹幕信息插入 施工中... by pren1
-3. 
+1. ✅ 监听所有vtuber直播间弹幕 已完成 --- by Kinori
+2. ✅ Python端构建数据库，并将弹幕信息插入 已完成 --- by pren1
+3. 弹幕信息优化：仅需要字数信息 施工中 --- by pren1
 
 
 
