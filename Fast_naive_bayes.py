5fa971fb6f3d6bbe15dcd2e2390ca21ca3d6ea56
ScilientHill <pren1@ucsc.edu>
2020-04-06 14:55:31 -0700

    proceeding

--- Fast_naive_bayes.py
+++ Fast_naive_bayes.py
@@ -3,6 +3,7 @@ import jieba
 import pdb
 import pandas as pd
 from tqdm import tqdm
+import random
 import numpy as np
 from sklearn.model_selection import train_test_split
 # seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
@@ -38,20 +39,47 @@ def train_naive_bayes(real_messages, fake_messages):
 
 	real_total_dict = train_empty_dict(total_dict.copy(), real_messages, sort=True)
 	fake_total_dict = train_empty_dict(total_dict.copy(), fake_messages, sort=True)
-	return real_total_dict, fake_total_dict
+	return real_total_dict, fake_total_dict, prior_of_real, prior_of_fake
+
+def shuffle_dataframe(df):
+	return df.sample(frac=1).reset_index(drop=True)
+
+def prob_calculation(prior, prob_dict, message):
+	'calculate the prob of one class'
+	res_prob = prior
+	for single_char in message:
+		res_prob *= prob_dict[single_char]
+	return res_prob
+
+def predict_message(real_total_dict, fake_total_dict, prior_of_real, prior_of_fake, message):
+	'naive bayes classifier'
+	real_prob = prob_calculation(prior_of_real, real_total_dict, message)
+	fake_prob = prob_calculation(prior_of_fake, fake_total_dict, message)
+	'then, we normalize these probabilities'
+	real_prob /= real_prob + fake_prob
+	fake_prob /= real_prob + fake_prob
+	return real_prob, fake_prob
 
 real_df = pd.read_csv('1.csv')
-real_messages = real_df['message']
-real_labels = np.ones(len(real_messages)).tolist()
+real_messages = shuffle_dataframe(real_df['message'])
 
 fake_df = pd.read_csv('0.csv')
-fake_messages = fake_df['message']
-fake_labels = np.zeros(len(fake_messages)).tolist()
+fake_messages = shuffle_dataframe(fake_df['message'])
 
-'combine these data'
-X = real_messages.copy().append(fake_messages)
-real_labels.extend(fake_labels)
-Y = real_labels
-assert len(X) == len(Y)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
-pdb.set_trace()
+'split train test sets'
+train_ratio = 0.8
+train_real_messages = real_messages[:int(len(real_messages) * train_ratio)]
+train_fake_messages = fake_messages[:int(len(fake_messages) * train_ratio)]
+
+test_real_messages = real_messages[int(len(real_messages) * train_ratio):]
+test_fake_messages = fake_messages[int(len(fake_messages) * train_ratio):]
+
+real_total_dict, fake_total_dict, prior_of_real, prior_of_fake = train_naive_bayes(train_real_messages, train_fake_messages)
+
+'Merge test cases'
+total_test_messages = test_real_messages.copy().append(test_fake_messages)
+total_test_labels = [1 for _ in test_real_messages] + [0 for _ in test_fake_messages]
+
+for (label, single_test_messages) in zip(total_test_labels, total_test_messages):
+	real_prob, fake_prob = predict_message(real_total_dict, fake_total_dict, prior_of_real, prior_of_fake, single_test_messages)
+	pdb.set_trace()
