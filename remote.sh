6f65bb558a1f67067fcb749aad8b340161e688a8
ScilientHill <pren1@ucsc.edu>
2020-04-21 15:00:56 -0700

    UPDATE

--- remote.sh
+++ remote.sh
@@ -6,5 +6,4 @@ scp update_data.py root@47.240.116.247:/root/DD_real_time_radar
 scp python_ws_client.py root@47.240.116.247:/root/DD_real_time_radar
 #scp trained_naive_bayes.npy root@47.240.116.247:/root/DD_real_time_radar
 scp Fast_naive_bayes.py root@47.240.116.247:/root/DD_real_time_radar
-
 # Vtuber-danmaku
